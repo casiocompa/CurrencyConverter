80929e577e0cccdd53dcf0074366392101a9909a
casiocompa <casiocompa@gmail.com>
2025-02-01 00:44:13 +0100

    Initial Commit

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Ruslan Kasian on 01.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
